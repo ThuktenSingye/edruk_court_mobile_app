42ddbb7efb115655592039cda3daaad84eb3217d
Tashi Tobgay <tashitobay179@gmail.com>
2025-04-29 22:05:46 +0600

    add

--- App.tsx
+++ App.tsx
@@ -6,19 +6,25 @@
  */
 
 import React from 'react';
-import {ScrollView, Text} from 'react-native';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {ScrollView, StyleSheet, Text} from 'react-native';
+import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
+import {COLORS} from './app/constant/designTokens';
 
 function App(): React.JSX.Element {
   return (
     <SafeAreaProvider>
-      <ScrollView>
-        <Text>Edruk Court</Text>
-      </ScrollView>
+      <SafeAreaView style={{flex: 1}}>
+        <Text style={styles.test}>Edruk Court</Text>
+      </SafeAreaView>
+      {/* <ScrollView></ScrollView> */}
     </SafeAreaProvider>
   );
 }
 
-// const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  test: {
+    color: COLORS.primary,
+  },
+});
 
 export default App;
