4b2e7281ac6489487b99ca87a45396c4450a8ebd
ThuktenSingye <thuktensingye2163@gmail.com>
2025-04-29 15:50:37 +0600

    Test Setup

---
+++ app/constant/designTokens.ts
@@ -0,0 +1,43 @@
+export const SIZES = {
+  xSmall: 10,
+  small: 12,
+  medium: 16,
+  large: 20,
+  xLarge: 24,
+  xxLarge: 32,
+};
+
+export const SHADOWS = {
+  small: {
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 2,
+  },
+  medium: {
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.25,
+    shadowRadius: 5.84,
+    elevation: 5,
+  },
+};
+
+export const FONTS = {
+  regular: 'LoraRegular',
+  medium: 'LoraMedium',
+  semiBold: 'LoraSemiBold',
+  bold: 'LoraBold',
+};
+
+export const COLORS = {
+  primary_light: '#197D41',
+  primary: '#046200',
+  secondary: '#1F316F',
+  background: '#E5E8EF',
+  textPrimary: '#0E1632',
+  textSecondary: '#323C47',
+  success: '#046200',
+  error: '#AC3030',
+};
